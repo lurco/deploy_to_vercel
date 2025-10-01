acce1a27c19a997227f2ab32d5192a801607407a
Andrzej Legutko <andrzejlegutko@gmail.com>
2025-10-01 08:38:24 +0200

    fix connection string to db

--- backend/db/db_init.py
+++ backend/db/db_init.py
@@ -2,6 +2,6 @@ from pymongo import AsyncMongoClient
 
 from config import settings
 
-client: AsyncMongoClient[dict] = AsyncMongoClient(settings.MONGO_CONNECTION_STRING)
+client: AsyncMongoClient[dict] = AsyncMongoClient(settings.MONGODB_URI)
 
 db = client.my_database
